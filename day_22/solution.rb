069ba2cdf7ebc9dcee3f62dbc22c0b4fed40e6b6
Stuart McHattie <stuart.mchattie@gmail.com>
2024-11-27 13:20:13 +0000

    Prepare an environment for Ruby solutions over 25 days

---
+++ day_22/solution.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def part1(input)
+  puts "Part 1\n======\n\n"
+end
+
+def part2(input)
+  puts "\nPart 2\n======\n\n"
+end
+
+input = File.readlines('input.txt').map { |number| number.to_i }
+
+part1(input)
+part2(input)
