5a2c9c76ec10d2ae5590158bba1845635ab4dd49
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-01 16:46:13 +0000

    Add solutions for day 1

--- day_01/solution.rb
+++ day_01/solution.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
 def part1(input)
-  puts "Part 1\n======\n\n"
+  puts "Part 1\n======\n"
+  left, right = input.transpose
+  left.sort!
+  right.sort!
+
+  left.zip(right).map { |l, r| (l - r).abs }.sum
 end
 
 def part2(input)
-  puts "\nPart 2\n======\n\n"
+  puts "\nPart 2\n======\n"
+  left, right = input.transpose
+
+  left.map { |l| l * right.count { |r| r == l } }.sum
 end
 
-input = File.readlines('input.txt').map { |number| number.to_i }
+input = File.readlines('input.txt').map { |line| line.split.map(&:to_i) }
 
-part1(input)
-part2(input)
+puts part1(input)
+puts part2(input)
