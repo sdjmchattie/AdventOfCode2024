1bc7c80b81dc168d79f871657cae016de56d52db
Stuart McHattie <sm49@sanger.ac.uk>
2024-12-04 10:02:28 +0000

    Simplify day 4 logic

--- day_04/solution.rb
+++ day_04/solution.rb
@@ -5,35 +5,27 @@ require_relative '../lib/grid/grid_2d.rb'
 def part1(input)
   puts "Part 1\n======\n"
 
-  words = input.width.times.flat_map do |x|
-    input.height.times.flat_map do |y|
-      DIRECTIONS.keys.map do |direction|
-        input.adjacent_values_in_direction(x, y, direction, including_self: true).join
+  input.width.times.sum do |x|
+    input.height.times.sum do |y|
+      next 0 unless input[x, y] == 'X'
+
+      DIRECTIONS.keys.count do |direction|
+        input.adjacent_values_in_direction(x, y, direction).join.start_with?('MAS')
       end
     end
   end
-
-  words.count { |word| word.start_with?('XMAS') }
 end
 
 def part2(input)
   puts "\nPart 2\n======\n"
 
   input.width.times.sum do |x|
-    input.height.times.sum do |y|
-      [:ne, :sw].count do |direction|
-        next unless input.adjacent_values_in_direction(x, y, direction, including_self: true).join.start_with?('MAS')
+    input.height.times.count do |y|
+      ne_word = input.adjacent_values_in_direction(x, y, :ne, including_self: true).join
+      next false unless ne_word.start_with?('MAS') || ne_word.start_with?('SAM')
 
-        if direction == :ne
-          next true if input.adjacent_values_in_direction(x, y - 2, :se, including_self: true).join.start_with?('MAS')
-          next true if input.adjacent_values_in_direction(x + 2, y, :nw, including_self: true).join.start_with?('MAS')
-        elsif direction == :sw
-          next true if input.adjacent_values_in_direction(x, y + 2, :nw, including_self: true).join.start_with?('MAS')
-          next true if input.adjacent_values_in_direction(x - 2, y, :se, including_self: true).join.start_with?('MAS')
-        end
-
-        false
-      end
+      se_word = input.adjacent_values_in_direction(x, y - 2, :se, including_self: true).join
+      se_word.start_with?('MAS') || se_word.start_with?('SAM')
     end
   end
 end
