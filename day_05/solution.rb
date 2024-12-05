41b3b244b6c3fec2d67713cd275a41bce6a5aa21
Stuart McHattie <sm49@sanger.ac.uk>
2024-12-05 09:23:21 +0000

    Add solutions for day 5

--- day_05/solution.rb
+++ day_05/solution.rb
@@ -1,14 +1,68 @@
 #!/usr/bin/env ruby
 
-def part1(input)
-  puts "Part 1\n======\n"
+class SparseArray
+  def initialize(value_type, *value_args)
+    @store = {}
+    @value_type = value_type
+    @value_args = value_args
+  end
+
+  def [](key)
+    @store[key] ||= @value_type.new(*@value_args)
+  end
+
+  def []=(key, value)
+    @store[key] = value
+  end
 end
 
-def part2(input)
+def pages_valid(rules, print_job)
+  print_job.each_with_index do |page, index|
+    print_job[index + 1..].each do |sub_page|
+      return false if rules[sub_page].include?(page)
+    end
+  end
+
+  true
+end
+
+def fix_order(rules, print_job)
+  print_job.map do |page|
+    [page, rules[page].count { |dependents| print_job.include?(dependents) }]
+  end.sort_by(&:last).map(&:first)
+end
+
+def part1(rules, print_jobs)
+  puts "Part 1\n======\n"
+  print_jobs.select { |job|
+    pages_valid(rules, job)
+  }.sum { |job|
+    job[(job.count - 1) / 2].to_i
+  }
+end
+
+def part2(rules, print_jobs)
   puts "\nPart 2\n======\n"
+  print_jobs.reject { |job|
+    pages_valid(rules, job)
+  }.map { |job|
+    fix_order(rules, job)
+  }.sum { |job|
+    job[(job.count - 1) / 2].to_i
+  }
 end
 
 input = File.readlines('input.txt')
+index = input.index("\n")
+order = input.shift(index)
+input.shift
+print_jobs = input.map { |line| line.chomp.split(',') }
 
-puts part1(input)
-puts part2(input)
+rules = SparseArray.new(Array)
+order.each do |line|
+  before, after = line.chomp.split('|')
+  rules[before] << after
+end
+
+puts part1(rules, print_jobs)
+puts part2(rules, print_jobs)
