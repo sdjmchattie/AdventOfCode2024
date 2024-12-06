7ad7edc037a82fbda466e6e92f2664e20c36e901
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-06 12:09:11 +0000

    Add solutions for day 6

--- day_04/solution.rb
+++ day_04/solution.rb
@@ -7,10 +7,11 @@ def part1(input)
 
   input.width.times.sum do |x|
     input.height.times.sum do |y|
-      next 0 unless input[x, y] == 'X'
+      point = Grid::Point2D.new(x, y)
+      next 0 unless input[point] == 'X'
 
       Grid::DIRECTIONS.keys.count do |direction|
-        input.adjacent_values_in_direction(x, y, direction).join.start_with?('MAS')
+        input.adjacent_values_in_direction(point, direction).join.start_with?('MAS')
       end
     end
   end
@@ -21,10 +22,10 @@ def part2(input)
 
   input.width.times.sum do |x|
     input.height.times.count do |y|
-      ne_word = input.adjacent_values_in_direction(x, y, :ne, including_self: true).join
+      ne_word = input.adjacent_values_in_direction(Grid::Point2D.new(x, y), :ne, including_self: true).join
       next false unless ne_word.start_with?('MAS') || ne_word.start_with?('SAM')
 
-      se_word = input.adjacent_values_in_direction(x, y - 2, :se, including_self: true).join
+      se_word = input.adjacent_values_in_direction(Grid::Point2D.new(x, y - 2), :se, including_self: true).join
       se_word.start_with?('MAS') || se_word.start_with?('SAM')
     end
   end
