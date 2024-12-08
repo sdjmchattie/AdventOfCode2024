e00412f4fa4ac3150b09ed587976f8e8c37adabb
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-08 13:46:10 +0000

    Add solutions for day 8

--- day_08/solution.rb
+++ day_08/solution.rb
@@ -1,14 +1,58 @@
 #!/usr/bin/env ruby
 
-def part1(input)
-  puts "Part 1\n======\n"
+require 'benchmark'
+require_relative '../lib/grid/grid_2d.rb'
+
+def part1(map)
+  antinodes = map.empty_dup
+  map.all_chars.each do |freq|
+    next if freq == '.'
+
+    locs = map.find(freq)
+    locs.each_with_index do |loc1, i|
+      locs[i + 1..].each do |loc2|
+        dx = loc2.x - loc1.x
+        dy = loc2.y - loc1.y
+        antinodes[Grid::Point2D.new(loc1.x - dx, loc1.y - dy)] = '#'
+        antinodes[Grid::Point2D.new(loc2.x + dx, loc2.y + dy)] = '#'
+      end
+    end
+  end
+
+  antinodes.count('#')
 end
 
-def part2(input)
-  puts "\nPart 2\n======\n"
+def part2(map)
+  antinodes = map.empty_dup
+  map.all_chars.each do |freq|
+    next if freq == '.'
+
+    locs = map.find(freq)
+    locs.each_with_index do |loc1, i|
+      locs[i + 1..].each do |loc2|
+        dx = loc2.x - loc1.x
+        dy = loc2.y - loc1.y
+
+        point = loc2
+        antinodes[point] = '#' while map.in_bounds?(point = Grid::Point2D.new(point.x - dx, point.y - dy))
+
+        point = loc1
+        antinodes[point] = '#' while map.in_bounds?(point = Grid::Point2D.new(point.x + dx, point.y + dy))
+      end
+    end
+  end
+
+  antinodes.count('#')
 end
 
-input = File.readlines('input.txt')
+contents = File.readlines('input.txt')
+map = Grid::Grid2D.new(contents)
 
-puts part1(input)
-puts part2(input)
+p1_result = nil
+p2_result = nil
+
+p1_time = Benchmark.realtime { p1_result = part1(map) } * 1000
+p2_time = Benchmark.realtime { p2_result = part2(map) } * 1000
+
+puts("Part 1 in #{p1_time.round(3)} ms\n  #{p1_result}\n\n")
+puts("Part 2 in #{p2_time.round(3)} ms\n  #{p2_result}\n\n")
