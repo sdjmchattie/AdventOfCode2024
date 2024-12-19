8a9541a4ab5a3fca53f7cee37cf9d8d3eb315e0e
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-19 20:49:11 +0000

    Add solutions for day 19

--- day_19/solution.rb
+++ day_19/solution.rb
@@ -2,18 +2,35 @@
 
 require 'benchmark'
 
-def part1(input)
+CACHE = {}
+
+def ways_to_create_pattern(pattern, towels)
+  return 1 if pattern.empty?
+  return CACHE[pattern] if CACHE.has_key?(pattern)
+
+  ways = towels.select { |towel| pattern.start_with?(towel) }.reduce(0) do |acc, towel|
+    acc + ways_to_create_pattern(pattern[towel.length..], towels)
+  end
+
+  CACHE[pattern] = ways
 end
 
-def part2(input)
+def part1(towels, patterns)
+  patterns.count { |pattern| ways_to_create_pattern(pattern, towels) > 0 }
+end
+
+def part2(towels, patterns)
+  patterns.sum { |pattern| ways_to_create_pattern(pattern, towels) }
 end
 
 input = File.readlines('input.txt')
+towels = input[0].chomp.split(', ')
+patterns = input[2..].map(&:chomp)
 
 p1_result = nil
-p1_time = Benchmark.realtime { p1_result = part1(input) } * 1000
+p1_time = Benchmark.realtime { p1_result = part1(towels, patterns) } * 1000
 puts("Part 1 in #{p1_time.round(3)} ms\n  #{p1_result}\n\n")
 
 p2_result = nil
-p2_time = Benchmark.realtime { p2_result = part2(input) } * 1000
+p2_time = Benchmark.realtime { p2_result = part2(towels, patterns) } * 1000
 puts("Part 2 in #{p2_time.round(3)} ms\n  #{p2_result}\n\n")
