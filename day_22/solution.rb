a78bd1df3f24bb33fc679be4b3c36efc6984528f
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-22 12:48:08 +0000

    Add solutions for day 22

--- day_22/solution.rb
+++ day_22/solution.rb
@@ -2,13 +2,43 @@
 
 require 'benchmark'
 
+def generate(secret)
+  result = secret
+  result = ((result << 6) ^ result) & 16777215
+  result = ((result >> 5) ^ result) & 16777215
+  ((result << 11) ^ result) & 16777215
+end
+
 def part1(input)
+  input.sum { |i| 2000.times.reduce(i) { |acc, _| generate(acc) } }
 end
 
 def part2(input)
+  change_map = input.map do |i|
+    sequence = [i] + 2000.times.map { i = generate(i) }
+    nanas = sequence.map { |v| v % 10 }
+
+    map = {}
+    last_four = []
+
+    (1...nanas.count).each do |j|
+      last_four.push(nanas[j] - nanas[j - 1])
+      last_four.shift if last_four.count == 5
+      map[last_four.dup] = map[last_four] || nanas[j] if last_four.count == 4
+    end
+
+    map
+  end
+
+  earnings = change_map.reduce({}) do |acc, m|
+    m.each_key { |k| acc[k] = (acc[k] || 0) + m[k] }
+    acc
+  end
+
+  earnings.values.max
 end
 
-input = File.readlines('input.txt')
+input = File.readlines('input.txt').map(&:to_i)
 
 p1_result = nil
 p1_time = Benchmark.realtime { p1_result = part1(input) } * 1000
