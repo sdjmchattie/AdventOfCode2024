8f5118d399de357105b7fccd5e33496714ad8a2a
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-24 17:19:15 +0000

    Add day 24 part 2 solution
    Partially by using code to create a plot of the circuit and partially by observing the output of the plot.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'pry'
+gem 'ruby-graphviz'
 
 group :lint do
   gem 'rubocop'
--- day_24/solution.rb
+++ day_24/solution.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'benchmark'
+require 'ruby-graphviz'
 
 class Gate
   attr_accessor :output_wire
@@ -81,90 +82,51 @@ def part1(input)
   get_z_value(gates)
 end
 
-def swap_outputs(gate_a, gate_b)
-  temp = gate_a.output_wire
-  gate_a.output_wire = gate_b.output_wire
-  gate_b.output_wire = temp
-
-end
-
-def test_system(x_sources, y_sources, gates)
-  false
-end
-
 def part2(input)
-  sources, gates = prepare_system(input)
-  x_sources = sources.select { |gate| gate.output_wire.start_with?('x') }.sort_by(&:output_wire)
-  y_sources = sources.select { |gate| gate.output_wire.start_with?('y') }.sort_by(&:output_wire)
+  # Not sure how to do this with code, so let's plot it and check the operations by hand!
+  g = GraphViz.new(:G, type: :digraph)
+  nodes = {}
 
-  # Horrible loop incoming
-  a1, a2, b1, b2, c1, c2, d1, d2 = (0..7).to_a
-  gc = gates.count
-  loop do
-    puts "#{a1} <-> #{a2}, #{b1} <-> #{b2}, #{c1} <-> #{c2}, #{d1} <-> #{d2}"
-
-    # Prepare the gates to swap
-    swapped_gates = [
-      gates[a1], gates[a2], gates[b1], gates[b2], gates[c1], gates[c2], gates[d1], gates[d2]
-    ]
-
-    # Do the swaps
-    (0..3).each { |i| swap_outputs(swapped_gates[i * 2], swapped_gates[i * 2 + 1]) }
-    gates.each { |gate| gate.connect_to_sources(sources + gates) }
-
-    # Test the system
-    return swapped_gates.map(&:output_wire).sort.join(',') if test_system(x_sources, y_sources, gates)
-
-    # Undo the swaps (no need to connect the sources as it'll be done again on the next swap)
-    (0..3).each { |i| swap_outputs(swapped_gates[i * 2], swapped_gates[i * 2 + 1]) }
-
-    # Increment the swap indices (urgh!)
-    loop do
-      d2 += 1
-
-      if d2 >= gc
-        d1 += 1
-        d2 = d1 + 1
-      end
-
-      if d1 >= gc
-        c2 += 1
-        d1 = d2 = c1 + 1
-      end
-
-      if c2 >= gc
-        c1 += 1
-        c2 = c1 + 1
-      end
-
-      if c1 >= gc
-        b2 += 1
-        c1 = c2 = b1 + 1
-      end
-
-      if b2 >= gc
-        b1 += 1
-        b2 = b1 + 1
-      end
-
-      if b1 >= gc
-        a2 += 1
-        b1 = b2 = a1 + 1
-      end
-
-      if a2 >= gc
-        a1 += 1
-        a2 = a1 + 1
-      end
-
-      if a1 >= gc
-        return
-      end
-
-      break unless a1 == a2 || a2 == b1 || b1 == b2 || b2 == c1 || c1 == c2 || c2 == d1 || d1 == d2 ||
-                   a1 >= gc || a2 >= gc || b1 >= gc || b2 >= gc || c1 >= gc || c2 >= gc || d1 >= gc || d2 >= gc
-    end
+  input.select { |e| e.include?(':') }.map do |e|
+    out_wire = e.match(/(...):/)[1]
+    nodes[out_wire] = g.add_nodes("Source: #{out_wire}", shape: :oval)
   end
+
+  input.select { |e| e.include?('->') }.map do |e|
+    gate_type, out_wire = e.match(/... ([A-Z]+) ... -> (...)/).captures
+    shape = {'AND' => :box, 'OR' => :diamond, 'XOR' => :trapezium}[gate_type]
+    nodes[out_wire] = g.add_nodes("#{gate_type}: #{out_wire}", shape:)
+  end
+
+  input.select { |e| e.include?('->') }.map do |e|
+    in_wire1, in_wire2, out_wire = e.match(/(...) [A-Z]+ (...) -> (...)/).captures
+    g.add_edges(nodes[in_wire1], nodes[out_wire])
+    g.add_edges(nodes[in_wire2], nodes[out_wire])
+  end
+
+  # Generate output image
+  g.output(:png => "circuit.png")
+end
+
+# Identified swaps to keep the logic consistent across the circuit:
+# ctg, rpb
+# dmh, z31
+# rpv, z11
+# dvq, z38
+
+def part2_test(input)
+  sources, gates = prepare_system(input)
+
+  x_sources = sources.select { |source| source.output_wire.start_with?('x') }.sort_by(&:output_wire)
+  x_value = x_sources.map(&:output_value).join.reverse.to_i(2)
+
+  y_sources = sources.select { |source| source.output_wire.start_with?('y') }.sort_by(&:output_wire)
+  y_value = y_sources.map(&:output_value).join.reverse.to_i(2)
+
+  z_gates = gates.select { |gate| gate.output_wire.start_with?('z') }.sort_by(&:output_wire)
+  z_value = z_gates.map(&:output_value).join.reverse.to_i(2)
+
+  "#{x_value} + #{y_value} = #{z_value} ?  #{x_value + y_value == z_value}"
 end
 
 input = File.readlines('input.txt')
@@ -176,3 +138,6 @@ puts("Part 1 in #{p1_time.round(3)} ms\n  #{p1_result}\n\n")
 p2_result = nil
 p2_time = Benchmark.realtime { p2_result = part2(input) } * 1000
 puts("Part 2 in #{p2_time.round(3)} ms\n  #{p2_result}\n\n")
+
+puts part2_test(input)
+puts ['ctg', 'rpb', 'dmh', 'z31', 'rpv', 'z11', 'dvq', 'z38'].sort.join(',')
