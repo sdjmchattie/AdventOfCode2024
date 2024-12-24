a515a14fe6b5665b412023f55a3ef5e21c6653cc
Stuart McHattie <stuart.mchattie@gmail.com>
2024-12-24 10:05:37 +0000

    Start putting a loop together for part 2
    I think it runs too slowly though

--- day_24/solution.rb
+++ day_24/solution.rb
@@ -3,7 +3,7 @@
 require 'benchmark'
 
 class Gate
-  attr_reader :output_wire
+  attr_accessor :output_wire
   attr_reader :source_gates
 
   def initialize(input_wires, output_wire)
@@ -54,7 +54,7 @@ class Xor < Gate
   end
 end
 
-def prepare_gates(input)
+def prepare_system(input)
   sources = input.select { |e| e.include?(':') }.map do |e|
     out_wire, out_value = e.match(/(...): ([01])/).captures
     Source.new(out_wire, out_value.to_i)
@@ -67,23 +67,104 @@ def prepare_gates(input)
 
   all_gates = sources + gates
   all_gates.each { |gate| gate.connect_to_sources(all_gates) }
-  all_gates
+
+  [sources, gates]
+end
+
+def get_z_value(gates)
+  z_gates = gates.select { |gate| gate.output_wire.start_with?('z') }.sort_by(&:output_wire)
+  z_gates.map(&:output_value).join.reverse.to_i(2)
 end
 
 def part1(input)
-  gates = prepare_gates(input)
+  _, gates = prepare_system(input)
+  get_z_value(gates)
+end
 
-  z_values = gates.select do |gate|
-    gate.output_wire.match?('z\d\d')
-  end.map do |gate|
-    [gate.output_wire, gate.output_value]
-  end.to_h
+def swap_outputs(gate_a, gate_b)
+  temp = gate_a.output_wire
+  gate_a.output_wire = gate_b.output_wire
+  gate_b.output_wire = temp
 
-  binary_num = z_values.keys.sort.reverse.map { |z| z_values[z] }.join
-  binary_num.to_i(2)
+end
+
+def test_system(x_sources, y_sources, gates)
+  false
 end
 
 def part2(input)
+  sources, gates = prepare_system(input)
+  x_sources = sources.select { |gate| gate.output_wire.start_with?('x') }.sort_by(&:output_wire)
+  y_sources = sources.select { |gate| gate.output_wire.start_with?('y') }.sort_by(&:output_wire)
+
+  # Horrible loop incoming
+  a1, a2, b1, b2, c1, c2, d1, d2 = (0..7).to_a
+  gc = gates.count
+  loop do
+    puts "#{a1} <-> #{a2}, #{b1} <-> #{b2}, #{c1} <-> #{c2}, #{d1} <-> #{d2}"
+
+    # Prepare the gates to swap
+    swapped_gates = [
+      gates[a1], gates[a2], gates[b1], gates[b2], gates[c1], gates[c2], gates[d1], gates[d2]
+    ]
+
+    # Do the swaps
+    (0..3).each { |i| swap_outputs(swapped_gates[i * 2], swapped_gates[i * 2 + 1]) }
+    gates.each { |gate| gate.connect_to_sources(sources + gates) }
+
+    # Test the system
+    return swapped_gates.map(&:output_wire).sort.join(',') if test_system(x_sources, y_sources, gates)
+
+    # Undo the swaps (no need to connect the sources as it'll be done again on the next swap)
+    (0..3).each { |i| swap_outputs(swapped_gates[i * 2], swapped_gates[i * 2 + 1]) }
+
+    # Increment the swap indices (urgh!)
+    loop do
+      d2 += 1
+
+      if d2 >= gc
+        d1 += 1
+        d2 = d1 + 1
+      end
+
+      if d1 >= gc
+        c2 += 1
+        d1 = d2 = c1 + 1
+      end
+
+      if c2 >= gc
+        c1 += 1
+        c2 = c1 + 1
+      end
+
+      if c1 >= gc
+        b2 += 1
+        c1 = c2 = b1 + 1
+      end
+
+      if b2 >= gc
+        b1 += 1
+        b2 = b1 + 1
+      end
+
+      if b1 >= gc
+        a2 += 1
+        b1 = b2 = a1 + 1
+      end
+
+      if a2 >= gc
+        a1 += 1
+        a2 = a1 + 1
+      end
+
+      if a1 >= gc
+        return
+      end
+
+      break unless a1 == a2 || a2 == b1 || b1 == b2 || b2 == c1 || c1 == c2 || c2 == d1 || d1 == d2 ||
+                   a1 >= gc || a2 >= gc || b1 >= gc || b2 >= gc || c1 >= gc || c2 >= gc || d1 >= gc || d2 >= gc
+    end
+  end
 end
 
 input = File.readlines('input.txt')
